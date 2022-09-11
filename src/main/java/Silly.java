62c92865565ad73407109bb78252159412215adc
j-y26 <jielin.yang@mail.utoronto.ca>
2022-09-11 17:22:01 -0400

    Complete TODO activities

--- src/main/java/Silly.java
+++ src/main/java/Silly.java
@@ -78,8 +78,16 @@ public class Silly implements Comparable<Silly>{
      *       Afterwards, set this.name to the concatenation of both
      *       strings (e.g. this.name = [first string] + [second string]).
      *       Make sure you document this method!
+     *
+     * Creates a new Silly object.
+     * This constructor takes in two Strings as arguments.
+     *
+     * @param s1 the first part of the name of this Silly instance.
+     * @param s2 the second part of the name of this Silly instance.
      */
-
+    public Silly(String s1, String s2){
+        this.name = s1 + s2;
+    }
 
 
 
@@ -116,7 +124,7 @@ public class Silly implements Comparable<Silly>{
         y.countStatic();
         x.countStatic();
         x.countStatic();
-        int[] expected_values = {};
+        int[] expected_values = {0, 1, 2, 3};
 
         System.out.println("The countStatic calls will return " + Arrays.toString(expected_values));
     }
@@ -134,6 +142,7 @@ public class Silly implements Comparable<Silly>{
     @Override
     public String toString(){
         // TODO (Task 3): Implement the body of this method!
+        return this.name;
     }
 
     /**
@@ -156,11 +165,14 @@ public class Silly implements Comparable<Silly>{
         if (!(o instanceof Silly)){
             return false;
         }
-
         Silly other = (Silly) o; // To access .name of o, we need to cast it.
 
         // Hint: to compare strings, we need to use .equals()
         //       e.g. s1.equals(s2)
+        if (this.name.equals(other.name)){
+            return true;
+        }
+        return false;
     }
 
     /**
@@ -194,6 +206,15 @@ public class Silly implements Comparable<Silly>{
          *                You can get the length of a string by using the
          *                .length() method.
          */
+        int myLength = this.name.length();
+        int otherLength = other.name.length();
+        if (myLength < otherLength) {
+            return -1;
+        } else if (myLength > otherLength) {
+            return 1;
+        } else {
+            return 0;
+        }
     }
 
     /*
